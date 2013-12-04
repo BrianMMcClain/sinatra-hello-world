3c5b269e183367c3faeed9615e75fd49b17f83c9
Brian McClain <brianmmcclain@gmail.com>
2013-12-04 10:34:58 -0500

    Add more output

--- app.rb
+++ app.rb
@@ -1,5 +1,5 @@
 require 'sinatra'
 
 get '/' do	
-	return ENV['VCAP_SERVICES'] + "<br /><br />\n\n" + ENV['VCAP_APPLICATION']
+	return ENV['VCAP_SERVICES'] + "<br /><br />\n\n" + ENV['VCAP_APPLICATION'] + "<br /><br />This was from git!"
 end
