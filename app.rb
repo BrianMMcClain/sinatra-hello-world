371ae2a02e91df9cda9350e80eb7fd9e79d19a91
Brian McClain <brianmmcclain@gmail.com>
2019-04-08 16:13:05 -0400

    Update / and add /env endpoints

--- app.rb
+++ app.rb
@@ -1,5 +1,26 @@
 require 'sinatra'
 
 get '/' do	
-	return ENV['VCAP_SERVICES'] + "<br /><br />\n\n" + ENV['VCAP_APPLICATION'] + "<br /><br />This was from git! Hello World!"
+	name = params["name"] || "World"
+	ret = <<-EOF
+	<html>
+		<head>
+			<title>Sinatra - Hello World!</title>
+		</head>
+		<body>
+			<h1>Hello, #{name}!</h1>
+		</body>
+	</html>
+	EOF
+
+	return ret
+end
+
+get '/env' do
+	ret = ""
+	ENV.keys.each do |k|
+		ret += "<b>#{k}</b>: #{ENV[k]}<br />"
+	end
+
+	return ret
 end
