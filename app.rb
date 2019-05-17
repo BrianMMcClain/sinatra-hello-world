e1746e7b7ae029c05465a37e195c1e2141b05aeb
Brian McClain <brianmmcclain@gmail.com>
2019-05-17 15:29:24 -0400

    Make instance IP and port easy to access

--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
 require 'sinatra'
+require 'json'
 
 get '/' do	
 	name = params["name"] || "World"
@@ -17,7 +18,13 @@ get '/' do
 end
 
 get '/env' do
-	ret = ""
+	internalIP = ENV["CF_INSTANCE_IP"] || "0.0.0.0"
+	internalPort = "0"
+	if not ENV["CF_INSTANCE_PORTS"].nil?
+		internalPort = JSON.parse(ENV["CF_INSTANCE_PORTS"])[0]["external_tls_proxy"]
+	end
+
+	ret = "<h1>#{internalIP}:#{internalPort}</h1><br /><br />"
 	ENV.keys.each do |k|
 		ret += "<b>#{k}</b>: #{ENV[k]}<br />"
 	end
